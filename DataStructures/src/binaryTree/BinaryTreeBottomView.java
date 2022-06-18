ea160c56baed4b5cc9919cc354edb8ce3bcbc6e8
AH32481 <AH32481@L3MSJP73.us.ad.wellpoint.com>
2022-06-18 23:57:21 +0530

    BinaryTreeBottomView Initial Commit

--- DataStructures/src/binaryTree/BinaryTreeBottomView.java
+++ DataStructures/src/binaryTree/BinaryTreeBottomView.java
@@ -1,5 +1,8 @@
 package binaryTree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BinaryTreeBottomView extends BinaryTree {
 	
 	public static void main(String[] args) {
@@ -9,7 +12,25 @@ public class BinaryTreeBottomView extends BinaryTree {
         tree.root.right = new Node(3);
         tree.root.left.left = new Node(4);
         tree.root.left.right = new Node(5);
+        printBottomView(tree.root);
        
+	}
+
+	private static void printBottomView(Node root) {
+		// TODO Auto-generated method stub
+		Queue<Node> q= new LinkedList<>();
+		q.add(root);
+		while(!q.isEmpty()) {
+			
+			Node temp=q.poll();
+			if(temp.left!=null)
+				q.add(temp.left);
+			if(temp.right!=null)
+				q.add(temp.right);
+			
+			if(temp.left==null&&temp.right==null)
+				System.out.println(temp.data);
+		}
 	}
 
 }
